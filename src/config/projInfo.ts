aec6616fadc22cf87e3e9ba96c8451bcaedff9f4
norlz <norlz@gitkra.com>
2024-12-26 21:30:16 +0800

    change await import to speed up..

--- src/config/projInfo.ts
+++ src/config/projInfo.ts
@@ -1,7 +1,34 @@
 import { ProjectInfo, ImgInfo } from '../projectInfoPanel/ProjectInfo';
 
+import ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_Demo_frame85_png from '../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp Demo frame85.png';
+import ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230519_1047_30182_LoadSaveFile_Procedure__png from '../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230519_1047_30182 LoadSaveFile Procedure .png';
+import ___assets_images_ori_copy_NttDrawAndChat_plantuml_pkg_drc_note_20230519_133347Z__png from '../assets/images_ori_copy/NttDrawAndChat/plantuml pkg drc note 20230519_133347Z .png';
+import ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_CodeFlow_png from '../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp_CodeFlow.png';
+import ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230518_2259_15767_NewUseSend__Plain___mp4 from '../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230518_2259_15767 NewUseSend (Plain) .mp4';
+import ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230518_2309_00326_ReceiveNetworkMsg__mp4 from '../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230518_2309_00326 ReceiveNetworkMsg .mp4';
+import ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230518_2322_26418_LoadSaveFile__mp4 from '../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230518_2322_26418 LoadSaveFile .mp4';
+import ___assets_images_ori_copy_TrafficSystem_TrafficSystem_Demo_png from '../assets/images_ori_copy/TrafficSystem/TrafficSystem Demo.png';
+import ___assets_images_ori_copy_TrafficSystem_TrafficSystem_20230609_003641_demo01_CrossBlock_Complex_mp4 from '../assets/images_ori_copy/TrafficSystem/TrafficSystem 20230609_003641 demo01_CrossBlock_Complex.mp4';
+import ___assets_images_ori_copy_TrafficSystem_TrafficSystem_20230609_004856_demo02_AlignedCube_ComplexMoving_TrafficLight_mp4 from '../assets/images_ori_copy/TrafficSystem/TrafficSystem 20230609_004856 demo02_AlignedCube_ComplexMoving_TrafficLight.mp4';
+import ___assets_images_ori_copy_TrafficSystemMock_TrafficSystemMock_Demo_20230714_230239Z_Intro__png from '../assets/images_ori_copy/TrafficSystemMock/TrafficSystemMock Demo 20230714_230239Z Intro .png';
+import ___assets_images_ori_copy_TrafficSystemMock_TrafficSystemMock_Demo_20230714_225153_Intro__mp4 from '../assets/images_ori_copy/TrafficSystemMock/TrafficSystemMock Demo 20230714_225153 Intro .mp4';
+import ___assets_images_ori_copy_TextHighlighter_TextHighlighter_Cev_20230528_demo_png from '../assets/images_ori_copy/TextHighlighter/TextHighlighter_Cev 20230528 demo.png';
+import ___assets_images_ori_copy_DrawOnWebpage_DrawOnWebpage_20230529_2347_41692_demo_png from '../assets/images_ori_copy/DrawOnWebpage/DrawOnWebpage 20230529_2347_41692 demo.png';
+import ___assets_images_ori_copy_code_comment_remover_ui_code_comment_remover_ui_demo_png from '../assets/images_ori_copy/code_comment_remover-ui/code_comment_remover-ui demo.png';
+import ___assets_images_ori_copy_code_comment_remover_ui_aws_Server_Jenkins_Docker_PrivateSubnet_ApiGateway___20231116_2001__template_png from '../assets/images_ori_copy/code_comment_remover-ui/aws_Server_Jenkins_Docker_PrivateSubnet_ApiGateway - 20231116_2001 .template.png';
+import ___assets_images_ori_copy_virtualfoldervsc_virtualfoldervsc_demo_20230930__png from '../assets/images_ori_copy/virtualfoldervsc/virtualfoldervsc demo 20230930 .png';
+import ___assets_images_ori_copy_TLightChat_TLightChat_demo___20231227_170152C__jpg from '../assets/images_ori_copy/TLightChat/TLightChat demo - 20231227_170152C .jpg';
+import ___assets_images_ori_copy_TLightChat_tlightchat_xstate_20231124_0230_11488_png from '../assets/images_ori_copy/TLightChat/tlightchat xstate 20231124_0230_11488.png';
+import ___assets_images_ori_copy_TLightChat_tlightchat_xstate_20231227_161601C_P1_png from '../assets/images_ori_copy/TLightChat/tlightchat xstate 20231227_161601C P1.png';
+import ___assets_images_ori_copy_TLightChat_tlightchat_xstate_20231227_161601C_P2_png from '../assets/images_ori_copy/TLightChat/tlightchat xstate 20231227_161601C P2.png';
+import ___assets_images_ori_copy_TLightChat_TLightChat_src_diagram_20231230_205627C_manual_org_webp from '../assets/images_ori_copy/TLightChat/TLightChat src_diagram 20231230_205627C manual_org.webp';
+import ___assets_images_ori_copy_TLightChat_TLightChat_demo___20231226_222723_speedup__mp4 from '../assets/images_ori_copy/TLightChat/TLightChat demo - 20231226_222723 speedup .mp4';
+import ___assets_images_ori_copy_infection_data_cov_it_demo___covid19_introTryTest___20231215_2331_56491__png from '../assets/images_ori_copy/infection_data_cov_it/demo - covid19_introTryTest - 20231215_2331_56491 .png';
+import ___assets_images_ori_copy_ocr_sentence_ui_demo___ocrSentence___20241219_172540567_png from '../assets/images_ori_copy/ocr-sentence-ui/demo - ocrSentence - 20241219_172540567.png';
+
 let seqAddedDate = 0;
 
+// new ImgInfo((await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp Demo frame85.png')).default, 2, 2),
 export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
   new ProjectInfo(
     'NttDrawAndChat',
@@ -12,16 +39,16 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp Demo frame85.png')).default, 2, 2),
-      new ImgInfo((await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230519_1047_30182 LoadSaveFile Procedure .png')).default),
-      new ImgInfo((await import('../assets/images_ori_copy/NttDrawAndChat/plantuml pkg drc note 20230519_133347Z .png')).default, 2, 2),
-      new ImgInfo((await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp_CodeFlow.png')).default, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_Demo_frame85_png, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230519_1047_30182_LoadSaveFile_Procedure__png),
+      new ImgInfo(___assets_images_ori_copy_NttDrawAndChat_plantuml_pkg_drc_note_20230519_133347Z__png, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_CodeFlow_png, 2, 2),
     ],
     [
       //
-      (await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230518_2259_15767 NewUseSend (Plain) .mp4')).default,
-      (await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230518_2309_00326 ReceiveNetworkMsg .mp4')).default,
-      (await import('../assets/images_ori_copy/NttDrawAndChat/DrawAndChatApp 20230518_2322_26418 LoadSaveFile .mp4')).default,
+      ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230518_2259_15767_NewUseSend__Plain___mp4,
+      ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230518_2309_00326_ReceiveNetworkMsg__mp4,
+      ___assets_images_ori_copy_NttDrawAndChat_DrawAndChatApp_20230518_2322_26418_LoadSaveFile__mp4,
     ],
     ++seqAddedDate,
     48, // lvOfComplexity
@@ -35,12 +62,12 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/TrafficSystem/TrafficSystem Demo.png')).default),
+      new ImgInfo(___assets_images_ori_copy_TrafficSystem_TrafficSystem_Demo_png),
     ],
     [
       //
-      (await import('../assets/images_ori_copy/TrafficSystem/TrafficSystem 20230609_003641 demo01_CrossBlock_Complex.mp4')).default,
-      (await import('../assets/images_ori_copy/TrafficSystem/TrafficSystem 20230609_004856 demo02_AlignedCube_ComplexMoving_TrafficLight.mp4')).default,
+      ___assets_images_ori_copy_TrafficSystem_TrafficSystem_20230609_003641_demo01_CrossBlock_Complex_mp4,
+      ___assets_images_ori_copy_TrafficSystem_TrafficSystem_20230609_004856_demo02_AlignedCube_ComplexMoving_TrafficLight_mp4,
     ],
     ++seqAddedDate,
     42, // lvOfComplexity
@@ -55,11 +82,11 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/TrafficSystemMock/TrafficSystemMock Demo 20230714_230239Z Intro .png')).default, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_TrafficSystemMock_TrafficSystemMock_Demo_20230714_230239Z_Intro__png, 2, 2),
     ],
     [
       //
-      (await import('../assets/images_ori_copy/TrafficSystemMock/TrafficSystemMock Demo 20230714_225153 Intro .mp4')).default,
+      ___assets_images_ori_copy_TrafficSystemMock_TrafficSystemMock_Demo_20230714_225153_Intro__mp4,
     ],
     ++seqAddedDate,
     55, // lvOfComplexity
@@ -73,7 +100,7 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/TextHighlighter/TextHighlighter_Cev 20230528 demo.png')).default),
+      new ImgInfo(___assets_images_ori_copy_TextHighlighter_TextHighlighter_Cev_20230528_demo_png),
     ],
     [
       //
@@ -89,7 +116,7 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/DrawOnWebpage/DrawOnWebpage 20230529_2347_41692 demo.png')).default),
+      new ImgInfo(___assets_images_ori_copy_DrawOnWebpage_DrawOnWebpage_20230529_2347_41692_demo_png),
     ],
     [
       //
@@ -107,8 +134,8 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/code_comment_remover-ui/code_comment_remover-ui demo.png')).default, 2, 2),
-      new ImgInfo((await import('../assets/images_ori_copy/code_comment_remover-ui/aws_Server_Jenkins_Docker_PrivateSubnet_ApiGateway - 20231116_2001 .template.png')).default),
+      new ImgInfo(___assets_images_ori_copy_code_comment_remover_ui_code_comment_remover_ui_demo_png, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_code_comment_remover_ui_aws_Server_Jenkins_Docker_PrivateSubnet_ApiGateway___20231116_2001__template_png),
     ],
     [
       //
@@ -126,7 +153,7 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/virtualfoldervsc/virtualfoldervsc demo 20230930 .png')).default),
+      new ImgInfo(___assets_images_ori_copy_virtualfoldervsc_virtualfoldervsc_demo_20230930__png),
     ],
     [
       //
@@ -144,15 +171,15 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/TLightChat/TLightChat demo - 20231227_170152C .jpg')).default, 2, 2),
-      new ImgInfo((await import('../assets/images_ori_copy/TLightChat/tlightchat xstate 20231124_0230_11488.png')).default, 2, 2),
-      new ImgInfo((await import('../assets/images_ori_copy/TLightChat/tlightchat xstate 20231227_161601C P1.png')).default),
-      new ImgInfo((await import('../assets/images_ori_copy/TLightChat/tlightchat xstate 20231227_161601C P2.png')).default),
-      new ImgInfo((await import('../assets/images_ori_copy/TLightChat/TLightChat src_diagram 20231230_205627C manual_org.webp')).default, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_TLightChat_TLightChat_demo___20231227_170152C__jpg, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_TLightChat_tlightchat_xstate_20231124_0230_11488_png, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_TLightChat_tlightchat_xstate_20231227_161601C_P1_png),
+      new ImgInfo(___assets_images_ori_copy_TLightChat_tlightchat_xstate_20231227_161601C_P2_png),
+      new ImgInfo(___assets_images_ori_copy_TLightChat_TLightChat_src_diagram_20231230_205627C_manual_org_webp, 2, 2),
     ],
     [
       //
-      (await import('../assets/images_ori_copy/TLightChat/TLightChat demo - 20231226_222723 speedup .mp4')).default,
+      ___assets_images_ori_copy_TLightChat_TLightChat_demo___20231226_222723_speedup__mp4,
     ],
     ++seqAddedDate,
     62, // lvOfComplexity
@@ -166,7 +193,7 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/infection_data_cov_it/demo - covid19_introTryTest - 20231215_2331_56491 .png')).default, 2, 2),
+      new ImgInfo(___assets_images_ori_copy_infection_data_cov_it_demo___covid19_introTryTest___20231215_2331_56491__png, 2, 2),
     ],
     [
       //
@@ -188,7 +215,7 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
       //
     ],
     ++seqAddedDate,
-    11, // lvOfComplexity
+    11 // lvOfComplexity
   ),
   new ProjectInfo(
     'ChromeCopy',
@@ -229,7 +256,7 @@ export const arr_ProjectInfo_ori: readonly ProjectInfo[] = [
     ],
     [
       //
-      new ImgInfo((await import('../assets/images_ori_copy/ocr-sentence-ui/demo - ocrSentence - 20241219_172540567.png')).default, 2, 1),
+      new ImgInfo(___assets_images_ori_copy_ocr_sentence_ui_demo___ocrSentence___20241219_172540567_png, 2, 1),
     ],
     [
       //
